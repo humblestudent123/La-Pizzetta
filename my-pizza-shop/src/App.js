82ebaf1d32fa9ec036450c8543f3650c3d9a4508
humblestudent123 <vasickinfedor@gmail.com>
2025-04-15 11:48:40 +0300

    1

--- my-pizza-shop/src/App.js
+++ my-pizza-shop/src/App.js
@@ -24,17 +24,36 @@ import pizzaImage from './images/f9df39e004c15247b2be6711bc3ec6dc.jpg';  // Им
 
 function App() {
   return (
-    <div style={{ 
-      backgroundImage: `url(${pizzaImage})`, 
-      backgroundSize: 'cover', 
-      backgroundPosition: 'center',
-      height: '300px',
-    }}>
-      <h1>Добро пожаловать в La Pizzetta!</h1>
-      <h2>La Pizzetta</h2>
-      <h3>"La Pizzetta — вкус, который приносит радость в каждый кусочек!"</h3>
-    </div>
+    // <div style={{ 
+    //   backgroundImage: `url(${pizzaImage})`, 
+    //   backgroundSize: 'cover', 
+    //   backgroundPosition: 'center',
+    //   height: '300',
+    //   margin: 0, // <- можно на всякий случай
+    //   padding: 0,
+    // }}>
+    //   <h1>Добро пожаловать в La Pizzetta!</h1>
+    //   <h2>La Pizzetta</h2>
+    //   <h3>"La Pizzetta — вкус, который приносит радость в каждый кусочек!"</h3>
+    // </div>
+
+
+    <div
+  className="App"
+  style={{
+    backgroundImage: `url(${pizzaImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '300px',
+  }}
+></div>
+
   );
 }
 
 export default App;
+
+
+
+
+
